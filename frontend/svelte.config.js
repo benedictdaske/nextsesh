c5d0fcb5ab5f00a83416afd05d25f8818f1febe7
benedictdaske <benedictdaske@tutanota.com>
2025-09-04 23:43:28 +0200

    adapt build config

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -2,6 +2,8 @@ import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [vitePreprocess()],
+
 	kit: {
 		adapter: adapter({
 			// See below for an explanation of these options
