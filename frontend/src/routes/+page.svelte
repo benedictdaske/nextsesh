df998a25ed317b5455da89aca60797f59e7e82ed
benedictdaske <benedictdaske@tutanota.com>
2025-09-05 00:13:25 +0200

    add teaser

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,8 +1,12 @@
 
 <div class="flex flex-col min-h-screen">
 
-    <div class="flex flex-1 h-1/3 justify-center items-center">
+    <div class="flex flex-1 h-1/3 justify-center items-end">
         <h1 class="text-6xl font-bold"> nextsesh </h1>
+    </div>
+    
+    <div class="flex flex-1 h-1/3 mt-2 justify-center items-start">
+        <h1 class="text-base italic font-extralight"> coming soon... </h1>
     </div>
     
 </div>
