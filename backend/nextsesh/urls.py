9f8eb2b9ebe4cb34e27bff69803d4e9a3b9a27b6
benedictdaske <benedictdaske@tutanota.com>
2025-09-10 11:09:41 +0200

    cahnge health route

--- backend/nextsesh/urls.py
+++ backend/nextsesh/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http import JsonResponse
+from django.http import HttpResponse
 
 def health(request):
-    return JsonResponse({'status': 'ok'})
+    return HttpResponse('OK')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
