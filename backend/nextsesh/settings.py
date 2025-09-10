37f037840efab0f21cf7a0930692caabc22432b4
benedictdaske <benedictdaske@tutanota.com>
2025-09-10 10:48:01 +0200

    update Dockerfile

--- backend/nextsesh/settings.py
+++ backend/nextsesh/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'nextsesh.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "NAME": os.environ.get("SQLITE_DB_PATH", BASE_DIR / "db.sqlite3"),
     }
 }
 
