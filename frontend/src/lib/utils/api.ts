d3b422766996b8f46476f2d6c88fff912bbc5406
benedictdaske <benedictdaske@tutanota.com>
2025-09-12 23:05:27 +0200

    restructure, multiple small changes before alpha launch

--- frontend/src/lib/utils/api.ts
+++ frontend/src/lib/utils/api.ts
@@ -4,12 +4,12 @@ import { get } from "svelte/store"
 const env = import.meta.env
 const API_ENDPOINT = env.VITE_API_ENDPOINT
 
-const accessToken = get(token)
-
 export async function callApi(api_path: string, options: RequestInit) {
+    const accessToken = get(token)
+    
     const headers = {
         ...options.headers,
-        "Authorization": `Bearer ${get(token)}`
+        "Authorization": `Bearer ${accessToken}`
     }
 
     const endpoint = API_ENDPOINT + api_path
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,19 @@
+<script lang="ts">
+	import { goto } from "$app/navigation"
+    import { auth } from "$lib/utils/auth-service"
+	import { isAuthenticated, user } from "$stores/auth-store";
+
+    const onschedule = () => {
+        goto('/schedule')
+    }
+    const onlogin = () => {
+        auth.loginWithRedirect()
+    }
+    const onlogout = () => {
+        auth.logout()
+    }
+</script>
+
 
 <div class="flex flex-col min-h-screen">
 
@@ -5,9 +21,21 @@
         <h1 class="text-6xl font-bold"> nextsesh </h1>
     </div>
     
-    <div class="flex flex-1 h-1/3 mt-2 justify-center items-start">
-        <h1 class="text-base italic font-extralight"> coming soon... </h1>
+    <div class="flex flex-1 h-1/3 mt-10 justify-center items-start">
+        <button onclick={onschedule} type="button" class="py-3 px-4 inline-flex items-center text-sm font-medium rounded-full border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+            Schedule
+        </button>
     </div>
+    
+    {#if $isAuthenticated}
+        <button onclick={onlogout} type="button" class="absolute top-4 right-4 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border border-transparent bg-red-400 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+            Logout
+        </button>
+    {:else}
+        <button onclick={onlogin} type="button" class="absolute top-4 right-4 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+            Login
+        </button>
+    {/if}
     
 </div>
 
